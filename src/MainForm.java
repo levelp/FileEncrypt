afda3e7ba09fcf6eaba36594aca0025e40e7b9ab
Denis Stepulenok <super.denis@gmail.com>
2016-01-14 20:47:50 +0300

    Создание окна

--- src/MainForm.java
+++ src/MainForm.java
@@ -8,10 +8,28 @@ public class MainForm {
     private JTextField dstFileName;
     private JButton encryptButton;
     private JPasswordField password;
+    private JPanel mainPanel;
 
     public MainForm() {
         encryptButton.addActionListener(actionEvent -> {
 
         });
+    }
+
+    public static void main(String[] args) {
+        // Создаём окно и задаём ему заголовок
+        JFrame frame = new JFrame("Шифрование / расшифровка файла с помощью XOR");
+        // Создаём класс, соответствующий нашей форме
+        MainForm mainForm = new MainForm();
+        // Основную панель помещаем внутрь окна
+        frame.setContentPane(mainForm.mainPanel);
+        // Размещение компонент внутри окна
+        // рекурсивное определение размеров компонент
+        frame.pack();
+        // Когда закрываем этот frame => закроется всё приложение
+        // Обычно задаётся для "основного" окна приложения
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        // Показываем окно
+        frame.setVisible(true);
     }
 }
