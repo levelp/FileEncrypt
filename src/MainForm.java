09d25a3aff15fe14a193020b11f1e9af66946ff9
Denis Stepulenok <super.denis@gmail.com>
2016-01-14 20:41:12 +0300

    Шифрование файлов (начальная форма)

---
+++ src/MainForm.java
@@ -0,0 +1,17 @@
+import javax.swing.*;
+
+/**
+ * Шифрование файлов
+ */
+public class MainForm {
+    private JTextField srcFileName;
+    private JTextField dstFileName;
+    private JButton encryptButton;
+    private JPasswordField password;
+
+    public MainForm() {
+        encryptButton.addActionListener(actionEvent -> {
+
+        });
+    }
+}
